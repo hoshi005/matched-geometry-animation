dbe54bcc7390c03566c5cb4c01762c6ccf440bca
Susumu Hoshikawa <hoshi005@me.com>
2020-10-18 13:43:49 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# matchedGeometryEffect と @Namespaceを使ったアニメーション
+
+## SwiftUI2.0で追加されたAPI
+
+SwiftUI2.0で追加された新機能のうち、Heroアニメーションを簡単に作れるAPIがあったのでちょっと触ってみました。
+
+## とりあえず完成形
+
+こんな感じのSegmentControlっぽいUIを作ってみます。
+
+![](images/0001.gif)
---
+++ anime/animeApp.swift
@@ -0,0 +1,17 @@
+//
+//  animeApp.swift
+//  anime
+//
+//  Created by Susumu Hoshikawa on 2020/10/18.
+//
+
+import SwiftUI
+
+@main
+struct animeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
